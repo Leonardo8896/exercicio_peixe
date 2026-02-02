d3ca55fe1f9d086b56a8ee52c000fea9ad0f3016
leonardo.afonso1 <leonardo.afonso@sou.inteli.edu.br>
2026-02-02 19:44:37 -0300

    add: resolve homework

---
+++ peixe.js
@@ -0,0 +1,39 @@
+//Você já tinha programado antes? Se sim, conte um pouco da sua experiência. Se essa foi a sua primeira experiência com código, conte o que você achou! Conseguiu entender todas as partes do tutorial? Tudo funcionou?
+
+var resposta = "Sim, fiz diversos projetos back-end com php, como um site de quiz para algumas aulas na etec, um jornal para a minha antiga escola e o back-end do meu TCC que foi baseado em uma arquitetura de micro-serviços php com API, WebSocket e um worker de rotina. Além disso, também tenho experiência em programação de sistema embarcados, projetando um sistema de incêndio autonomo e uma estação portátil de tratamento de água, interigados a um aplicativo, como meu TCC";
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+
+    scene: {
+        preload:preload,
+        create: create,
+        update: update
+    }
+}
+
+var game = new Phaser.Game(config);
+
+var peixinho;
+
+function preload() {
+    this.load.image('mar', 'assets/bg_azul-escuro.png');
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
+    this.load.image('peixe', 'assets/peixes/peixinho_laranja.png');
+}
+
+function create() {
+    this.add.image(400, 300, 'mar');
+    this.add.image(400, 525, 'logo').setScale(0.5);
+
+    peixinho = this.add.image(400, 300, 'peixe');
+
+    peixinho.setFlip(true, false);
+}
+
+function update() {
+    peixinho.x = this.input.x;
+    peixinho.y = this.input.y;
+}
