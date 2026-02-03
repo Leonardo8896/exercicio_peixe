33d6c4bbe88cf9481af07eb9b6e96faeb1e015b0
leonardo.afonso1 <leonardo.afonso@sou.inteli.edu.br>
2026-02-03 07:45:21 -0300

    refactor: solve extra tasks

--- peixe.js
+++ peixe.js
@@ -1,6 +1,6 @@
-//Você já tinha programado antes? Se sim, conte um pouco da sua experiência. Se essa foi a sua primeira experiência com código, conte o que você achou! Conseguiu entender todas as partes do tutorial? Tudo funcionou?
+let resposta = "Sim, fiz diversos projetos back-end com php, como um site de quiz para algumas aulas na etec, um jornal para a minha antiga escola e o back-end do meu TCC que foi baseado em uma arquitetura de micro-serviços php com API, WebSocket e um worker de rotina. Além disso, também tenho experiência em programação de sistema embarcados, projetando um sistema de incêndio autonomo e uma estação portátil de tratamento de água, interigados a um aplicativo, como meu TCC";
 
-var resposta = "Sim, fiz diversos projetos back-end com php, como um site de quiz para algumas aulas na etec, um jornal para a minha antiga escola e o back-end do meu TCC que foi baseado em uma arquitetura de micro-serviços php com API, WebSocket e um worker de rotina. Além disso, também tenho experiência em programação de sistema embarcados, projetando um sistema de incêndio autonomo e uma estação portátil de tratamento de água, interigados a um aplicativo, como meu TCC";
+console.log(resposta);
 
 var config = {
     type: Phaser.AUTO,
@@ -22,18 +22,34 @@ function preload() {
     this.load.image('mar', 'assets/bg_azul-escuro.png');
     this.load.image('logo', 'assets/logo-inteli_branco.png');
     this.load.image('peixe', 'assets/peixes/peixinho_laranja.png');
+    this.load.image('coral', 'assets/coral.webp');
 }
 
 function create() {
     this.add.image(400, 300, 'mar');
     this.add.image(400, 525, 'logo').setScale(0.5);
+    this.add.image(400, 400, 'coral').setScale(0.4);
 
     peixinho = this.add.image(400, 300, 'peixe');
 
     peixinho.setFlip(true, false);
 }
 
+let lastX = 0;
+let x = 0;
+
 function update() {
-    peixinho.x = this.input.x;
+    x = this.input.x;
+    if(x !== lastX) {
+        if(x-lastX > 0) {
+            peixinho.setFlip(true, false);
+        } else {
+            peixinho.setFlip(false, false);
+        }
+    }
+    peixinho.x = x;
+
     peixinho.y = this.input.y;
+
+    lastX = x;
 }
